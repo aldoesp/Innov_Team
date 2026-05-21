ff526e362eb0fe56e3b05a601e682bc438745e5e
Jou09 <tanbrunell@gmail.com>
2026-05-21 08:59:56 +0200

    Final

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Info from './components/info/info';
+import EquipeInnovation from './components/equipe-innovation/EquipeInnovation';
 import './App.css';
 
 function App() {
   return (
 
     <div className="app-container">
+      <EquipeInnovation />
       <Info />
     </div>
 
