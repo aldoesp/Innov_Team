fbaf548aa517b3c30740fafdbb6e6b449b76ebcd
maxjohn81 <rivoandriharisoa@gmail.com>
2026-05-21 10:15:21 +0300

    oui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import Info from './components/info/info';
-import EquipeInnovation from './components/equipe-innovation/EquipeInnovation';
 import './App.css';
 
 function App() {
   return (
 
     <div className="app-container">
-      <EquipeInnovation />
       <Info />
     </div>
 
